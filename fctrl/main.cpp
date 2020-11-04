941b4ffa3608426961af8c31eb07dfe1259e0a40
liember <liember@yandex.ru>
2020-11-05 06:06:07 +0700

    test argpars

--- fctrl/main.cpp
+++ fctrl/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include "argpars.hpp"
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
+    argpars line(argc, argv);
+    auto params = line.GetParams();
+    for (auto &&i : params)
+    {
+        std::cout << i << endl;
+    }
+
+    std::cout << std::endl;
+
+    auto args = line.GetArgs();
+    for (auto &&i : args)
+    {
+        std::cout << i << endl;
+    }
     return 0;
 }
