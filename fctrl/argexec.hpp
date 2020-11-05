81455556cf161c0eb85b1ce2d1af3ebe795e657b
liember <liember@yandex.ru>
2020-11-06 05:58:12 +0700

    fix IDE include bug

---
+++ fctrl/argexec.hpp
@@ -0,0 +1,17 @@
+#ifndef __ARGEXEC_H__
+#define __ARGEXEC_H__
+
+#include <map>
+#include <utility>
+#include "argpars.hpp"
+
+class argexec : argpars
+{
+private:
+    /* data */
+public:
+    argexec(/* args */);
+    ~argexec();
+};
+
+#endif // __ARGEXEC_H__
