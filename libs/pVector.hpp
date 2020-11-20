d2fa4687b73a1bb99ed008cd3b94c891251b7c08
liember <liember@yandex.ru>
2020-11-20 21:28:06 +0700

    make copy safe pVector

--- libs/pVector.hpp
+++ libs/pVector.hpp
@@ -1,48 +1,80 @@
 #pragma once
 
 #include "vector"
+#include "memory"
+#include "exceptions.hpp"
 
 template<class T>
 class pVector {
 private:
-    std::vector<T *> storage;
+    std::vector<T *> *storage;
     T **res;
-    bool released;
+    bool *released;
+    unsigned *ref_counter;
+
 public:
     pVector() {
-        released = false;
+        ref_counter = new unsigned;
+        *ref_counter = 1;
+        storage = new std::vector<T *>;
+        released = new bool;
+        *released = false;
     }
 
+    pVector(const pVector &src) {
+        ref_counter = src.ref_counter;
+        *ref_counter += 1;
+        storage = src.storage;
+        released = src.released;
+        res = src.res;
+        release();
+    }
+
+    pVector &operator=(const pVector &a) { return *this; }
+
     ~pVector() {
-        if (released)
-            delete res;
-        for (auto &&i : storage) {
-            delete i;
+        if (*ref_counter <= 1) {
+            if (released)
+                delete res;
+            for (auto &&i : *storage) {
+                delete i;
+            }
+            delete ref_counter;
+            delete storage;
+            delete released;
+        } else {
+            *ref_counter--;
         }
     }
 
     auto push_back(T *element) {
-        if (!released) {
-            storage.push_back(element);
+        if (!*released) {
+            storage->push_back(element);
             return true;
         }
         return false;
     }
 
-    auto size(){
-        return storage.size();
-    }
-
-    T** release() {
-        if (!released) {
-            released = true;
-            auto size = storage.size();
+    void release() {
+        if (!*released) {
+            *released = true;
+            auto size = storage->size();
             res = new T *[size + 1];
+            std::vector<T *> &vecRef = *storage;
             for (size_t i = 0; i < size; i++) {
-                res[i] = storage[i];
+                res[i] = vecRef[i];
             }
             res[size] = (T *) nullptr;
         }
-        return res;
+    }
+
+    auto size() const {
+        return storage->size();
+    }
+
+    auto get() const {
+        if (*released)
+            return res;
+        throw Except("Unreleased pVector cant give pointer to vector");
     }
 };
--- test/pVector.cpp
+++ test/pVector.cpp
@@ -5,10 +5,12 @@
 #include "gtest/gtest.h"
 #include "pVector.hpp"
 
+const static std::string a[] = {"word1", "word2", "word3", "word4", "word5"};
+
 TEST(pVector, memory) {
     auto vec = new pVector<char>;
     char *w1;
-    for (int i = 0; i < 10000000; ++i) {
+    for (int i = 0; i < 1000; ++i) {
         w1 = new char[100];
         strcpy(w1, "word");
         vec->push_back(w1);
@@ -17,18 +19,77 @@ TEST(pVector, memory) {
 }
 
 TEST(pVector, release) {
-    std::string a[] = {"word1", "word2", "word3", "word4", "word5"};
     auto vec = new pVector<char>;
     char *w1;
-    for (int i = 0; i < 5; ++i) {
+    for (auto &i : a) {
         w1 = new char[100];
-        strcpy(w1, a[i].c_str());
+        strcpy(w1, i.c_str());
         vec->push_back(w1);
     }
-    auto rel = vec->release();
+    vec->release();
     for (int i = 0; i < vec->size(); ++i) {
-        ASSERT_STREQ(rel[i],a[i].c_str());
+        ASSERT_STREQ(vec->get()[i], a[i].c_str());
     }
     ASSERT_NO_THROW(delete vec);
 }
 
+TEST(pVector, copy_released) {
+    auto vec1 = new pVector<char>;
+    char *w1;
+    for (const auto &i : a) {
+        w1 = new char[100];
+        strcpy(w1, i.c_str());
+        vec1->push_back(w1);
+    }
+    vec1->release();
+
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_STREQ(a[i].c_str(), vec1->get()[i]);
+    }
+
+    pVector<char> vec2(*vec1);
+    delete vec1;
+
+    auto chk = vec2.get();
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_STREQ(a[i].c_str(), chk[i]);
+    }
+}
+
+TEST(pVector, copy_unreleased) {
+    auto vec1 = new pVector<char>;
+    char *w1;
+    for (const auto &i : a) {
+        w1 = new char[100];
+        strcpy(w1, i.c_str());
+        vec1->push_back(w1);
+    }
+
+    pVector<char> vec2(*vec1);
+    delete vec1;
+    vec2.release();
+    auto chk = vec2.get();
+    for (int i = 0; i < 5; ++i) {
+        ASSERT_STREQ(a[i].c_str(), chk[i]);
+    }
+}
+
+pVector<char> foo() {
+    pVector<char> res;
+    char *w1;
+    for (const auto &i : a) {
+        w1 = new char[100];
+        strcpy(w1, i.c_str());
+        res.push_back(w1);
+    }
+    res.release();
+    return res;
+}
+
+TEST(pVector, return_from_function) {
+    auto vec = foo();
+    auto res = vec.get();
+    for (int i = 0; i < vec.size(); ++i) {
+        ASSERT_STREQ(res[i], a[i].c_str());
+    }
+}
