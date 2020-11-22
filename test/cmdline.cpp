b867ef0ac09f11c8f32848f770fab35982aad08c
liember <liember@yandex.ru>
2020-11-23 02:09:21 +0700

    test string stream

--- test/cmdline.cpp
+++ test/cmdline.cpp
@@ -5,26 +5,38 @@
 #include "gtest/gtest.h"
 #include "cmdline/cmdline.hpp"
 #include <string>
-#include <strstream>
+#include "strstream"
 
 auto buf_size = 2059;
 std::string name = "shell";
 auto test_string = "hello it arguments for cmdline test";
 
-TEST(cmdline,init){
+TEST(cmdline, init) {
     cmdline shell(name, buf_size);
-    ASSERT_STREQ(shell.getName().data(),name.c_str());
+    ASSERT_STREQ(shell.getName().data(), name.c_str());
 }
 
-TEST(cmdline,dyn_init){
-    char * tst_name = new char[20];
-    strcpy(tst_name,name.c_str());
+TEST(cmdline, dyn_init) {
+    char *tst_name = new char[20];
+    strcpy(tst_name, name.c_str());
     cmdline shell(tst_name, buf_size);
     delete[] tst_name;
-    ASSERT_STREQ(shell.getName().data(),name.c_str());
+    ASSERT_STREQ(shell.getName().data(), name.c_str());
 }
 
-TEST(cmdline,get_cv){
+TEST(cmdline, string_stream) {
+    std::string str = "hello world";
+    cmdline sh(name.c_str(), buf_size);
+    std::stringstream ss(str);
+    sh.input(ss);
+    auto v = sh.get_v();
+    ASSERT_EQ(sh.size(),2);
+    ASSERT_STREQ(v[0].c_str(),"hello");
+    ASSERT_STREQ(v[1].c_str(),"world");
+
+}
+
+TEST(cmdline, get_cv) {
     cmdline shell(name, buf_size);
     std::stringstream ss1(test_string);
     std::stringstream ss2(test_string);
